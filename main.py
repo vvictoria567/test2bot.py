3b0d3ffbbab9fb5e337d0f64fe757c027b1c2e2a
vvictoria567 <victoriavykhrystyuk@gmail.com>
2026-05-22 15:33:26 +0300

    Update main.py(flask2)

--- main.py
+++ main.py
@@ -522,16 +522,18 @@ app = Flask(__name__)
 def home():
     return "Bot is running"
 
-def run_bot():
-    asyncio.run(main())
 
-if __name__ == "__main__":
-
-    threading.Thread(target=run_bot).start()
-
+def start_flask():
     port = int(os.environ.get("PORT", 10000))
 
     app.run(
         host="0.0.0.0",
         port=port
     )
+
+
+if __name__ == "__main__":
+
+    threading.Thread(target=start_flask).start()
+
+    asyncio.run(main())
